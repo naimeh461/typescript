ac9e894daefedf5e9a6c33e70828433bd86736bb
naimeh461 <naimeh461@gmail.com>
2023-11-29 22:06:44 +0600

    first commit

---
+++ src/1.4.ts
@@ -0,0 +1,34 @@
+{//Basic Data Type
+
+    //string
+    let firstName: string = "Naime"
+
+    //number
+    let roll: number = 123
+
+    //boolean
+    let isAdmin: boolean = true
+
+    // undefined
+    let x: undefined = undefined
+
+    //null
+    let y: null = null
+
+// 
+    let d; // any type like javascript
+
+    d = 123
+    d = "ddd"
+    d = true
+
+    //Array
+
+    let friends: string[] = ['rachel', 'monica']
+    let eligibleRollList: number[] = [1, 3, 4,]
+
+
+    //tuple --> array --> order --> type of values
+    let coordinates: [number, number] = [1, 5]
+    let ageName: [number, string, boolean] = [50, "Mr.X", true]
+}
